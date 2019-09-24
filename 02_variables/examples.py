8315d766040c9d32364344c1fc8c1ab08e828566
silent-mobius <alex@vaiolabs.com>
2019-09-24 15:06:54 +0300

    adding examples

---
+++ 02_variables/examples.py
@@ -0,0 +1,9 @@
+name='Alex'
+age=42
+lname="Bino"
+gpa=4.9
+address="Somewhere over the rainbow"
+size_of_pi=3.14 # atlease the beggining
+size_of_byte=8 #bit
+Engaged = True #yes --< its a boolean
+Married False #ring was for bday
