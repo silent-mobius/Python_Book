e843339cf13363cda3e2efc242e38356f7931f3b
silent-mobius <alex@vaiolabs.com>
2019-09-25 03:32:50 +0300

    removing old fields

--- 06_loops/ex1.py
+++
@@ -1,6 +0,0 @@
-var = int(input("Please insert number: "))
-i =0 
-
-while i< 20 :
-    print(var,i)
-    i  += 1 # i = i+1
--- 06_loops/ex3.py
+++
@@ -1,9 +0,0 @@
-i = int(input("enter nnumber :"))
-
-if i>1000:
-    print(i ,"is bigger then 1000")
-else:
-    while i < 1000:
-        if i%2==1:
-            print(i)
-        i+=1
