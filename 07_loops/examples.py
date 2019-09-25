a76249dd42031f164997d85665e1012db2df4a5c
silent-mobius <alex@vaiolabs.com>
2019-09-25 04:35:23 +0300

    adding exaple file

---
+++ 07_loops/examples.py
@@ -0,0 +1,39 @@
+var = int(input("Please insert number: "))
+i =0 
+
+while i< 20 :
+    print(var,i)
+    i  += 1 # i = i+1
+
+
+
+i = int(input("Please insert number :"))
+
+if i>1000:
+    print(i,"is bigger then 1000")
+else:
+    while i < 1000:
+        print(i)
+        i+=1
+
+
+
+i = int(input("enter nnumber :"))
+
+if i>1000:
+    print(i ,"is bigger then 1000")
+else:
+    while i < 1000:
+        if i%2==1:
+            print(i)
+        i+=1
+
+
+
+import time
+x=int(input('insert number: '))
+i=0
+while i< x:
+    print('*'*i,)
+    i = i+1
+    time.sleep(0.5)
