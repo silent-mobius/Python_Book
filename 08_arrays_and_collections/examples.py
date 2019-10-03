435678d2a0d0380e4ff5b3bff20951d262a34992
pushtakio <alex@vaiolabs.com>
2019-10-03 23:14:29 +0300

    adding exaples

--- 08_arrays_and_collections/examples.py
+++ 08_arrays_and_collections/examples.py
@@ -14,3 +14,17 @@ for val in numbers:
 
 # Output: The sum is 48
 print("The sum is", sum)
+
+
+#-----
+# Output: range(0, 10)
+print(range(10))
+
+# Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(range(10)))
+
+# Output: [2, 3, 4, 5, 6, 7]
+print(list(range(2, 8)))
+
+# Output: [2, 5, 8, 11, 14, 17]
+print(list(range(2, 20, 3)))
