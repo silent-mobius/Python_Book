9d35c21ac2350709db9d5de8128bb544bdaabf7e
pushtakio <alex@vaiolabs.com>
2019-10-05 22:13:16 +0300

    adding examples

--- 03_data_types/examples.py
+++ 03_data_types/examples.py
@@ -372,3 +372,156 @@ for x in range(10):
 >>> [x+y for x in ['Python ','C '] for y in ['Language','Programming']]
 ['Python Language', 'Python Programming', 'C Language', 'C Programming']
 
+
+
+my_list = ['p','r','o','b','l','e','m']
+# Output: True
+print('p' in my_list)
+# Output: False
+print('a' in my_list)
+# Output: True
+print('c' not in my_list)
+
+
+
+for fruit in ['apple','banana','mango']:
+    print("I like",fruit)
+
+
+# Empty tuple
+my_tuple = ()
+print(my_tuple)  # Output: ()
+
+# Tuple having integers
+my_tuple = (1, 2, 3)
+print(my_tuple)  # Output: (1, 2, 3) 
+
+# tuple with mixed datatypes
+my_tuple = (1, "Hello", 3.4)
+print(my_tuple)  # Output: (1, "Hello", 3.4)  
+
+# nested tuple
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+
+# Output: ("mouse", [8, 4, 6], (1, 2, 3)) 
+print(my_tuple)
+
+
+
+my_tuple = 3, 4.6, "dog"
+print(my_tuple)   # Output: 3, 4.6, "dog" 
+
+# tuple unpacking is also possible
+a, b, c = my_tuple
+
+print(a)      # 3
+print(b)      # 4.6 
+print(c)      # dog 
+
+
+my_tuple = ("hello")
+print(type(my_tuple))  # <class 'str'>
+
+# Creating a tuple having one element
+my_tuple = ("hello",)  
+print(type(my_tuple))  # <class 'tuple'> 
+
+# Parentheses is optional
+my_tuple = "hello",
+print(type(my_tuple))  # <class 'tuple'> 
+
+
+
+my_tuple = ('p','e','r','m','i','t')
+
+print(my_tuple[0])   # 'p' 
+print(my_tuple[5])   # 't'
+
+# IndexError: list index out of range
+# print(my_tuple[6])
+
+# Index must be an integer
+# TypeError: list indices must be integers, not float
+# my_tuple[2.0]
+
+# nested tuple
+n_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+
+# nested index
+print(n_tuple[0][3])       # 's'
+print(n_tuple[1][1])       # 4
+
+
+my_tuple = ('p','e','r','m','i','t')
+
+# Output: 't'
+print(my_tuple[-1])
+
+# Output: 'p'
+print(my_tuple[-6])
+
+
+
+my_tuple = ('p','y','t','h','o','n',' ','c','o','u','r','s','e')
+
+# elements 2nd to 4th
+# Output: ('r', 'o', 'g')
+print(my_tuple[1:4])
+
+# elements beginning to 2nd
+# Output: ('p', 'r')
+print(my_tuple[:-7])
+
+# elements 8th to end
+# Output: ('i', 'z')
+print(my_tuple[7:])
+
+# elements beginning to end
+# Output: ('p','y','t','h','o','n',' ','c','o','u','r','s','e')
+print(my_tuple[:])
+
+
+my_tuple = (4, 2, 3, [6, 5])
+# TypeError: 'tuple' object does not support item assignment
+# my_tuple[1] = 9
+
+# However, item of mutable element can be changed
+my_tuple[3][0] = 9    # Output: (4, 2, 3, [9, 5])
+print(my_tuple)
+
+# Tuples can be reassigned
+my_tuple = ('p','y','t','h','o','n',' ','c','o','u','r','s','e')
+
+# Output: ('p','y','t','h','o','n',' ','c','o','u','r','s','e')
+print(my_tuple)
+
+
+
+# Concatenation
+# Output: (1, 2, 3, 4, 5, 6)
+print((1, 2, 3) + (4, 5, 6))
+
+# Repeat
+# Output: ('Repeat', 'Repeat', 'Repeat')
+print(("Repeat",) * 3)
+
+
+my_tuple = ('p','y','t','h','o','n',' ','c','o','u','r','s','e')
+
+# can't delete items
+# TypeError: 'tuple' object doesn't support item deletion
+# del my_tuple[3]
+
+# Can delete an entire tuple
+del my_tuple
+
+# NameError: name 'my_tuple' is not defined
+print(my_tuple)
+
+
+
+my_tuple = ('a','p','p','l','e',)
+print(my_tuple.count('p'))  # Output: 2
+print(my_tuple.index('l'))  # Output: 3
+
+##################################################################
