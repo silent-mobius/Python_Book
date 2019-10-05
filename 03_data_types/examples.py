6a462d6ea68db4fc13867082605915dac691c5ed
pushtakio <alex@vaiolabs.com>
2019-10-05 21:40:14 +0300

    adding examples

--- 03_data_types/examples.py
+++ 03_data_types/examples.py
@@ -31,3 +31,87 @@ x = bool(5) 	#bool
 x = bytes(5) 	#bytes 	
 x = bytearray(5) 	#bytearray 	
 x = memoryview(bytes(5)) 	#memoryview 	
+
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# all of the following are equivalent
+my_string = 'Hello'
+print(my_string)
+
+my_string = "Hello"
+print(my_string)
+
+my_string = '''Hello'''
+print(my_string)
+
+# triple quotes string can extend multiple lines
+my_string = """Hello, welcome to
+           the world of Python"""
+print(my_string)
+
+
+str = 'programiz'
+print('str = ', str)
+
+#first character
+print('str[0] = ', str[0])
+
+#last character
+print('str[-1] = ', str[-1])
+
+#slicing 2nd to 5th character
+print('str[1:5] = ', str[1:5])
+
+#slicing 6th to 2nd last character
+print('str[5:-2] = ', str[5:-2])
+
+
+# index must be in range
+>>> my_string[15]  
+...IndexError: string index out of range
+# index must be an integer
+>>> my_string[1.5] 
+...TypeError: string indices must be integers
+...TypeError: 'str' object doesn't support item deletion
+>>> del my_string
+>>> my_string
+...NameError: name 'my_string' is not defined
+
+
+
+str1 = 'Hello'
+str2 ='World!'
+
+# using +
+print('str1 + str2 = ', str1 + str2)
+
+# using *
+print('str1 * 3 =', str1 * 3)
+
+
+    >>> 'Hello ''World!'
+    'Hello World!'
+    >>> # using parentheses
+    >>> s = ('Hello '
+    ...      'World')
+    >>> s
+    'Hello World'
+
+    
