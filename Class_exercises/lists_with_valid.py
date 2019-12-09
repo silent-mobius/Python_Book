6e7aae21a6d1d413ece0c6abb78cef3aa1fdce56
Silent-Mobius <alex@vaiolabs.com>
2019-12-09 22:09:45 +0200

    adding list validation script

---
+++ Class_exercises/lists_with_valid.py
@@ -0,0 +1,17 @@
+'''תגדירו רשימה חדשה בשם פירות
+תבקשו ממשתמש שיכניס פירות לתוך רשמה עד שיהיו 7 פירות ברשימה
+אם ערך של פרי מסוים חוזר על עצמו יותר מפעם אחת, אסור להכניס אותו
+לרשימה קיימת ויש צורך להוציא למשתמש הודעת שגיאה אבל בכל זאת להמשיך להריץ את התוכנה
+'''
+
+fruits = []
+
+while len(fruits) < 7:
+    fruit = input("Please enter fruit: ")
+    if fruit in fruits:
+        print(fruit, " is already in list, please try again")
+        continue
+    else:
+        fruits.append(fruit)
+
+print(len(fruits), fruits)
