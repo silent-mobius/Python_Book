a342e662878c4a39cbbbe3549fcb1755beaded09
Silent-Mobius <alex@vaiolabs.com>
2019-12-12 12:51:00 +0200

    typo fix

--- Class_exercises/reading_files.py
+++ Class_exercises/reading_files.py
@@ -3,14 +3,12 @@ with open('story.txt', 'r') as file_data:
     data = file_data.readlines()
 
 
-word_to_print=[]
+word_to_print = []
 
 for word in range(len(data)):
     if word % 4 == 0:
-        word_to_print.append(data[word])
-    
+        word_to_print.append(data[word])   
 
-word_to_print=set(word_to_print)
+word_to_print = set(word_to_print)
 
-print(word_to_print,end='')
-
+print(word_to_print, end='')
