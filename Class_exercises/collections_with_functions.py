8b058bde96da306a8bf7c7331c62181ec2527152
Pushtakio <alex@vaiolabs.com>
2019-12-22 15:15:07 +0200

    adding hw work

---
+++ Class_exercises/collections_with_functions.py
@@ -0,0 +1,63 @@
+data1=[1,2,3,[1,2,3,4,5,6],[[1,2],[3,4],[5,6]],((1,2),(3,4))]
+data2={1:['a','b','c',5,6,7],2:[5,6,7,'c','b','a'],3:(1,2),4:(3,4)}
+data3=(5,5,'a','b','c','a','c',5,'5','z','g','x')
+
+#1.1 # 1.2
+
+def shortest_list_in_collections(lst):
+    short = len(lst)
+    short_list =  []
+    new_list = []
+
+    for i in lst:
+        try:
+            if type(i) == type(list()):
+                for j in i:
+                    if type(j) == type(list()):
+                        if len(j) < short:
+                            short_list.append(j)
+        except:
+            print('error happened')
+
+        for i in short_list:
+            new_list = new_list + i
+
+    return new_list
+
+    
+def print_collection(lst):
+    for i in lst:
+        print(i)
+
+
+tmp_data = shortest_list_in_collections(data1)
+
+print(tmp_data)
+
+
+
+#2.1 #2.2
+new_tuple= []
+for i in data3:
+    if i not in new_tuple:
+        new_tuple.append(i)
+
+new_tuple = tuple(new_tuple)
+
+for i in new_tuple:
+    print(i,end=' ')
+
+# 3
+dict_element = data2[2]
+print()
+
+for k in dict_element:
+
+    tmp = ord(str(k))
+    tmp = tmp + 1
+    lock = dict_element.index(k)
+    tmp = chr(tmp)
+    dict_element.insert(lock,tmp)
+    dict_element.pop(lock+1 )
+
+print(dict_element)
