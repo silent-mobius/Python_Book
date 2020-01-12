e494da9e28c332ab3fa5d8d2b9b09aec20c26838
Pushtakio <alex@vaiolabs.com>
2020-01-12 10:50:16 +0200

    adding class work

---
+++ Class_exercises/f_length.py
@@ -0,0 +1,14 @@
+def f_length(string,string2):
+    if len(string) == len(string2):
+        return 0
+    else:
+        return 1
+
+var = f_length('kaka', 'baba')
+
+print(var)
+
+
+var = f_length('kuro shitake', 'ahoi akuma')
+
+print(var)
