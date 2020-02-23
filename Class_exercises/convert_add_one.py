b2352964444366cb3a13a4c401828b4ca5664f08
Alex M. Schapelle <que@vaio.labs>
2020-02-23 22:10:56 +0200

    adding convert exercise

--- Class_exercises/convert_add_one.py
+++ Class_exercises/convert_add_one.py
@@ -3,13 +3,24 @@ x = 'f'
 
 def add_one(x):
     try:
-        if type(x) is str:
+        if type(x) is str and  str(x).isalpha():
             tmp = ' '
             tmp = x
             x = ord(tmp)
             x = x+1
             x = chr(x)
             return x
+        
+        if type(x) is str and  str(x).isalnum():
+            tmp = ' '
+            tmp = x[-1]
+            if tmp == 'z' or tmp == 'Z':
+                tmp= tmp - 26
+            x = ord(tmp)
+            x = x+1
+            x = chr(x)
+            return x
+        
         else:
             return x+1
     except:
