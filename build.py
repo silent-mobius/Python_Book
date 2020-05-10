e8e1ff53660fbbd28047aeccfc7c283c05348e28
Pushtakio <alex@vaiolabs.com>
2020-05-10 09:08:57 +0300

    adding build script

--- build.py
+++ build.py
@@ -8,19 +8,44 @@
 ########################################################################
 
 import os
+import os.path
 import re
 import sys
+import subprocess
 import datetime
-
 from zipfile import ZipFile
 
-today = datetime.date.today()
+try:
+    import pypdf
+except ImportError:
+    print("[-] no python library: ")
+    print('[+] Trying to install the library')
+    os.system('pip3 install pypdf')
+#folders = os.listdir()
+
+def folder_list(path):
+    folder_list=[]
+    for file_ in path:
+        if file_.startswith(('0','1','2','9')):
+            folder_list.append(file_)
+            
+    return folder_list
 
 
-def get_all_files_into_zip(dirs):
+def create_md_file(folders,dest_file):
 
-    paths=[]
+    for folder in folders:
+        os.chdir(folder)
+        with open('README.md')
 
 
-
-    for root, directories, files  in os.walk(dirs)
+'''
+for i in n:
+     ...:     os.chdir(i)
+     ...:     with open('README.md') as infile:
+     ...:         d = infile.readlines()
+     ...:         
+     ...:     os.chdir('../')
+     ...:     with open('tmp.txt','a') as nfile:
+     ...:          nfile.writelines(d)
+'''
